35adccba02ca731297932b2450c5f9d3afa19429
the-king-123 <thekingluffy123@gmail.com>
2024-09-02 21:28:42 +0300

    m

--- src/app/forum/preview/[slug]/previewForum.js
+++ src/app/forum/preview/[slug]/previewForum.js
@@ -12,7 +12,7 @@ function previewForum({ forum }) {
 
     return (
         <div>
-            <div style={{padding:8}}>
+            <div style={{ padding: 8 }}>
                 {forum.type == "image" &&
                     <Image
                         id="postImageMedia"
@@ -36,9 +36,19 @@ function previewForum({ forum }) {
                 <div className='w3-big w3-large w3-margin-top'>
                     {forum.title}
                 </div>
-                <div style={{marginBlock:16}}>
-                    {forum.content}
+                <div className='postCore'>
+                    <div id='post0' style={{ marginBlock: 16 }} className='w3-overflow w3-nowrap-multiline'>
+                        {forum.content}
+                    </div>
                 </div>
+                {
+                    JSON.parse(forum.response).map((response, key) => (
+                        <div key={key} className='w3-border-left w3-text-grey' style={{ paddingInline: 16, paddingBlock: 4, marginBlock: 4 }}>
+                            {response}
+                        </div>
+                    ))
+                }
+
             </div>
         </div>
     )
