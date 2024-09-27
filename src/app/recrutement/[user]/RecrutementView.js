1bee0e156b7bdd34f59ebd74aaa32d1bb951a443
the-king-123 <thekingluffy123@gmail.com>
2024-09-27 13:16:15 +0300

    m

--- src/app/recrutement/[user]/RecrutementView.js
+++ src/app/recrutement/[user]/RecrutementView.js
@@ -7,8 +7,19 @@ import React from 'react'
 function RecrutementView(props) {
     console.log(props.view);
 
-    const month = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+    const nLangue = [
+        '1 sur 6 = A1 (Débutant)',
+        '2 sur 6 = A2 (Intermédiaire)',
+        '3 sur 6 = B1 (Pré-intermédiaire)',
+        '4 sur 6 = B2 (Intermédiaire avancé)',
+        ' 5 sur 6 = C1 (Avancé)',
+        '5 sur 6 = C2 (Maîtrise)',
+    ]
 
+    const month = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+    const moneyMaker = (number) => {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    }
     const dateMaker = (date) => {
         const dateBrut = date.split('-');
         return dateBrut[2].slice(0, 2) + ' ' + month[dateBrut[1] * 1 - 1] + ' ' + dateBrut[0]
@@ -22,18 +33,43 @@ function RecrutementView(props) {
 
                     <div className="w3-large w3-big">{props.view.fullname}</div>
                     <div className="w3-text-grey">{props.view.title}</div>
-                    <div className="w3-border-bottom" style={{paddingBlock:4}}>
-                        <div>{props.view.adress}</div>
-                        <div>{props.view.contact}</div>
+                    <div className="w3-border-bottom w3-border-top w3-margin-top" style={{ paddingBlock: 8 }}>
+                        <div className="w3-text-grey">{props.view.adresse}</div>
+                        <div className="w3-text-grey">{props.view.contact}</div>
                     </div>
-                    <div className="w3-border-bottom" style={{paddingBlock:4}}>
-                        <div>{props.view.psalarial}</div>
-                        <div>Niveau de langue : {JSON.parse(props.view.nlangue).fr}</div>
-                        <div>{props.view.dactylot && 'Je sais taper sans regarder le clavier'}</div>
+                    <div className="w3-border-bottom" style={{ paddingBlock: 8 }}>
+                        <div>Prétention salarial : {moneyMaker(props.view.psalarial)}</div>
+                        <div>Niveau de langue : {nLangue[JSON.parse(props.view.nlangue).fr * 1 - 1]}</div>
+                        <div>{props.view.dactylot == 'true' && 'Je sais taper sans regarder le clavier'}</div>
                     </div>
-                    <div style={{paddingBlock:4}}>
-                        <div>{parse(props.view.content)}</div>
+                    <div style={{ paddingBlock: 8 }}>
+                        <div className="w3-light-grey w3-round" style={{ padding: 16 }}>{parse(props.view.content)}</div>
                     </div>
+
+
+                    <div className="w3-text-grey w3-small">Postulée le {dateMaker(props.view.updated_at)}</div>
+
+                    <div className="w3-margin-top w3-round w3-light-grey" style={{ padding: 8,marginBlock:8 }}>Photo CIN :</div>
+                    <Image
+                        alt={props.view.title}
+                        unoptimized
+                        loading="lazy"
+                        height={240}
+                        width={320}
+                        src={
+                            source +
+                            "/images.php?w=320&h=320&zlonk=6733&zlink=" +
+                            props.view.cin
+                        }
+                        style={{
+                            objectPosition: "center",
+                            objectFit: "cover",
+                            zIndex: 1,
+                        }}
+                        className="w3-light-grey w3-round"
+                    />
+
+                    <div className="w3-margin-top w3-round w3-light-grey" style={{ padding: 8,marginBlock:8 }}>CV :</div>
 
                     <Image
                         alt={props.view.title}
@@ -44,7 +80,7 @@ function RecrutementView(props) {
                         src={
                             source +
                             "/images.php?w=620&h=620&zlonk=6733&zlink=" +
-                            props.view.cin
+                            props.view.cv
                         }
                         style={{
                             objectPosition: "center",
@@ -53,10 +89,6 @@ function RecrutementView(props) {
                         }}
                         className="w3-light-grey post-image"
                     />
-
-                    <div>{props.view.cin}</div>
-                    <div>{props.view.cv}</div>
-                    <div>{dateMaker(props.view.updated_at)}</div>
                 </div>
 
             }
