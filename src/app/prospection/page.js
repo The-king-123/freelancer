5cbc115e150b0b59fa19b1536113fd57e846ca1d
TheKing Shaffid <thekingluffy123@gmail.com>
2024-12-13 08:57:13 +0100

    m

--- src/app/prospection/page.js
+++ src/app/prospection/page.js
@@ -1,10 +1,40 @@
-'use client'
-import React, { useEffect } from 'react'
+import { notFound } from "next/navigation";
+import axios from "axios";
+import Home from "@/app/Home/page";
+import {app_name, console_source as source} from "@/app/data";
+import Store from "./Prospection";
 
-function page() {
-    useEffect(() => {
-        window.location = '/prospection/all'
-    }, [])
+export default async function page({ params }) {
+
+  try {
+    // var products = ''
+    const products = await axios
+      .get(source + "/_store/all?where=prospection")
+      .then((res) => {
+        return res.data.data;
+      })
+      .catch((e) => {
+        console.error("failure", e);
+      });
+    if (!products) {
+      notFound();
+    }
+    return <Home core={<Store products={products} />} />;
+  } catch (error) {
+    console.error(`Error rendering products for user ${params.category}:`, error);
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>There was an error loading the products. Please try again later.</p>
+      </div>
+    );
+  }
 }
 
-export default page
+export function metadata() {
+  const meta = {
+    title: app_name,
+    description: 'Visiter la meilleure boutique numérique',
+  };
+  return meta;
+}
